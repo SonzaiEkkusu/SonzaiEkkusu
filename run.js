dc0647be4ec80e0c47db5d3a1d52171f1e7f8717
SonzaiX シ <abhiedthea@gmail.com>
2024-08-05 20:09:33 +0700

    Update run.js

--- run.js
+++ run.js
@@ -1,4 +1,8 @@
-console.log('=== Panel Siap Digunakan ===')
+console.log('🚀=== Panel Siap Digunakan ===🚀');
+console.log('📂 Silahkan unggah file bot mu di tab File 📂');
+console.log('🌟 Images Docker by Sonzai X シ');
+console.log('📱 Telegram: November2k');
+console.log('💻 GitHub: github.com/SonzaiEkkusu');
 require('child_process').spawn('bash', [], {
   stdio: ['inherit', 'inherit', 'inherit', 'ipc']
-})
+});
